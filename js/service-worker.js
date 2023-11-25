6c96cd638262d1b99f6f1a9f48ff7d71b4482ce5
Raphael Rouiller <rrouille@c1r1s7.42lausanne.ch>
2023-11-25 12:17:38 +0100

    Added the possibility of installing the web application

--- js/service-worker.js
+++ js/service-worker.js
@@ -2,9 +2,9 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/index.html',
-        '/css/styles.css',
-        '/scripts/script.js',
+        './index.html',
+        './css/styles.css',
+        './js/main.js',
       ]);
     })
   );
