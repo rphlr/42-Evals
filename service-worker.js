080e54c1c41a498736dd3fdd4195b64263395b12
Raphael Rouiller <rrouille@c1r1s7.42lausanne.ch>
2023-11-25 10:25:30 +0100

    Added favicon

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,25 @@
-self.addEventListener('install', (e) => {
-	console.log('[Service Worker] Install');
-	
-  });
-  
-  self.addEventListener('fetch', (e) => {
-	console.log('[Service Worker] Fetch', e.request.url);
-  });
-  
+self.addEventListener('install', function(event) {
+  // Précachage de ressources
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/stylesheet/styles.css',
+        '/scripts/script.js',
+        // Autres ressources nécessaires
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Cache hit - retourne la réponse du cache
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
