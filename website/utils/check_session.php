9f2b7a69143aa2c8818c5b939f266ecb585f3992
rrouille <rrouille@rrouille.local>
2024-01-08 20:17:49 +0100

    Some redirections after login failed...

--- website/utils/check_session.php
+++ website/utils/check_session.php
@@ -2,17 +2,27 @@
 session_start();
 
 $excludedExtensions = array('.otf', '.jpg', '.png', '.css', '.js');
+$excludedPaths = array('/js/', '/srcs/', '/fonts/', '/images/');
 $isResourceRequest = false;
 
+$requestUri = $_SERVER['REQUEST_URI'];
+
 foreach ($excludedExtensions as $ext) {
-    if (substr($_SERVER['REQUEST_URI'], -strlen($ext)) === $ext) {
+    if (substr($requestUri, -strlen($ext)) === $ext) {
+        $isResourceRequest = true;
+        break;
+    }
+}
+
+foreach ($excludedPaths as $path) {
+    if (strpos($requestUri, $path) === 0) {
         $isResourceRequest = true;
         break;
     }
 }
 
 if (!$isResourceRequest && !isset($_SESSION['user'])) {
-    $_SESSION['redirect_url'] = $_SERVER['REQUEST_URI'];
+    $_SESSION['redirect_url'] = $requestUri;
     header("Location: ../login");
     exit();
 }
