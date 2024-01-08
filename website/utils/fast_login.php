7efe4fcea0b485b7d74201e87804c27c95596f15
Raphael Rouiller <rrouille@c3r4s4.42lausanne.ch>
2024-01-08 10:14:19 +0100

    Well, that's sad, tomorrow is the last day :'(

--- website/utils/fast_login.php
+++ website/utils/fast_login.php
@@ -1,8 +1,8 @@
 <?php
 $dbservername = $_SERVER['REDIRECT_DB_SERVER'];
-$dbname = $_SERVER['REDIRECT_DB_FLOGIN_USERNAME'];
-$username = $_SERVER['REDIRECT_DB_FLOGIN_PASSWORD'];
-$dbpass = $_SERVER['REDIRECT_DB_FLOGIN_NAME'];
+$dbname = $_SERVER['REDIRECT_DB_FLOGIN_NAME'];
+$username = $_SERVER['REDIRECT_DB_FLOGIN_USERNAME'];
+$dbpass = $_SERVER['REDIRECT_DB_FLOGIN_PASSWORD'];
 
 $conn = new mysqli($dbservername, $username, $dbpass, $dbname);
 
@@ -12,12 +12,9 @@ if ($conn->connect_error) {
 
 $sql = "INSERT INTO UserActivity (timestamp) VALUES (NOW())";
 
-if ($conn->query($sql) === TRUE) {
-	header("Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-	exit();
-} else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
+$conn->query($sql);
 $conn->close();
+
+header("Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+exit();
 ?>
