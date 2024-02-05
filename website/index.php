c68aeb635c7675368b0957491b7e13841bd24ca0
Raphael <rrouille@student.42lausanne.ch>
2024-02-05 14:16:44 +0100

    Automated index update with stargazer info and timestamp

--- website/index.php
+++ website/index.php
@@ -268,17 +268,17 @@ require(PATH . "utils/menu.php");
 					<h3 class="font-weight-bold mb-1">Funny Stats 📊</h3>
 					<center>
 						<b>Last stargazer:</b>
-						<p><!--last_stargazer_start_HTML--><a href="https://github.com/irrivero"><img
-                  src="https://avatars.githubusercontent.com/u/116060502?v=4&s=250"
+						<p><!--last_stargazer_start_HTML--><a href="https://github.com/erickramosxp"><img
+                  src="https://avatars.githubusercontent.com/u/113641652?v=4&s=250"
                   target="_blank"></a><!--last_stargazer_end_HTML-->
 						</p>
-						<p>Thanks to <!--name_start_HTML--><a href="https://github.com/irrivero">irrivero</a><!--name_end_HTML-->
+						<p>Thanks to <!--name_start_HTML--><a href="https://github.com/erickramosxp">erickramosxp</a><!--name_end_HTML-->
 							for starring the repository! 🎉</p>
 					</center>
 					<b>Star Count: <a href="https://github.com/rphlr/42-Evals"
-							target="_blank"><!--stars_start-->514<!--stars_end--></a> 🌟</b>
+							target="_blank"><!--stars_start-->515<!--stars_end--></a> 🌟</b>
 					<br />
-					<p><b>Updates occur hourly. Last update :</b> <!--date_start_HTML-->05.02.2024, 13:28:02<!--date_end_HTML-->
+					<p><b>Updates occur hourly. Last update :</b> <!--date_start_HTML-->05.02.2024, 14:16:44<!--date_end_HTML-->
 						(CET)</p>
 					<center>
 						<div>
