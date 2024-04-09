fae5cecb12a3a918b12616d912428c02e0f1c66b
Raphael <rrouille@student.42lausanne.ch>
2024-04-09 10:19:29 +0200

    Automated index update with stargazer info and timestamp

--- website/index.php
+++ website/index.php
@@ -278,7 +278,7 @@ require(PATH . "utils/menu.php");
 					<b>Star Count: <a href="https://github.com/rphlr/42-Evals"
 							target="_blank"><!--stars_start-->599<!--stars_end--></a> 🌟</b>
 					<br />
-					<p><b>Updates occur hourly. Last update :</b> <!--date_start_HTML-->09.04.2024, 08:16:00<!--date_end_HTML-->
+					<p><b>Updates occur hourly. Last update :</b> <!--date_start_HTML-->09.04.2024, 09:19:28<!--date_end_HTML-->
 						(CET)</p>
 					<center>
 						<div>
