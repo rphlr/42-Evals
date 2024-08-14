a4b8d5780002ba67080eeb04f3ec48ad99e9bde2
Raphael Rouiller <rr@104-0-0-104.lightspeed.wchtks.sbcglobal.net>
2024-08-14 14:50:54 +0200

    Stable release

--- components/Header.jsx
+++ components/Header.jsx
@@ -93,13 +93,13 @@ function Header() {
                         anchor="bottom end"
                         className="w-52 origin-top-right rounded-xl border border-sky-500 border-opacity-20 bg-gray-700 bg-opacity-800 p-2 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
                     >
-                        <MenuItem>
-                            {/* <Link href='/admin' className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'>
+                        {/* <MenuItem>
+                            <Link href='/admin' className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'>
                                 <button className="">
                                     Admin Panel
                                 </button>
-                            </Link> */}
-                        </MenuItem>
+                            </Link>
+                        </MenuItem> */}
                         <MenuItem>
                             <Link href='/sheets' className='group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10'>
                                 <button className="">
