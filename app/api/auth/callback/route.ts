e1db213202ec080c71e1d635257c8f58544cea51
Raphael Rouiller <rr@104-0-0-104.lightspeed.wchtks.sbcglobal.net>
2024-08-16 17:40:09 +0200

    Add 42 login

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/app/libs/prismadb';
+import { Prisma } from '@prisma/client';
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
