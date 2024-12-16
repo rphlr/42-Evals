b89fb154e2779a0278d4f2e1a34941ff75ec08b3
Raphael <rrouille@student.42lausanne.ch>
2024-12-16 08:19:51 +0100

    Automated index update with stargazer info and timestamp

--- components/homecomponents/FunnyStats.jsx
+++ components/homecomponents/FunnyStats.jsx
@@ -50,24 +50,24 @@ function FunnyStats() {
                         Last stargazer:
                     </p>
                     <div className="flex justify-center items-center">
-                        {/* last_stargazer_start_JSX */}<a href="https://github.com/loreeue"><img src="https://avatars.githubusercontent.com/u/116285503?v=4&s=250"
+                        {/* last_stargazer_start_JSX */}<a href="https://github.com/amyaby"><img src="https://avatars.githubusercontent.com/u/113394974?v=4&s=250"
                   target="_blank"/></a>{/* last_stargazer_end_JSX */}
                     </div>
 
                     <p className=' pt-2 text-gray-600 text-center'>
                         Thanks to
                         {/* name_start_JSX */} <a
-                            href="https://github.com/loreeue"
+                            href="https://github.com/amyaby"
                             target="_blank"
                             className='text-[#0D94B6] hover:underline'
-                        >loreeue</a> {/* name_end_JSX */}
+                        >amyaby</a> {/* name_end_JSX */}
                         for starring the repository! 🎉
                     </p>
                     <p className=' pt-2 text-gray-600 font-bold '>
-                        Star Count: <span className='text-[#0D94B6] font-bold'>{/* stars_start */}830{/* stars_end */}</span> 🌟
+                        Star Count: <span className='text-[#0D94B6] font-bold'>{/* stars_start */}831{/* stars_end */}</span> 🌟
                     </p>
                     <p className=' pt-2 text-gray-600 font-bold '>
-                        Updates occur hourly. Last update : <span className=' font-normal'> {/* date_start_JSX */}16.12.2024, 07:28:45{/* date_end_JSX */} (CET)</span>
+                        Updates occur hourly. Last update : <span className=' font-normal'> {/* date_start_JSX */}16.12.2024, 08:19:50{/* date_end_JSX */} (CET)</span>
                     </p>
                     <a href="https://github.com/rphlr/42-Evals" target="_blank"><img src="https://api.star-history.com/svg?repos=rphlr/42-Evals&type=Date&" className='mt-5' alt="rphlr's Blue Github Chart" /></a>
                     <hr className='my-5' />
